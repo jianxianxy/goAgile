a18c36bc0009232fa811300013a38dfd40e64efc
886665 <zhao.zhiwei@sf-express.com>
2017-11-02 15:22:50 +0800

    goAgile

--- src/controller/demo.go
+++ src/controller/demo.go
@@ -4,16 +4,33 @@ import (
 	"net/http"
     "fmt"
 )
-func Demo(reponse http.ResponseWriter, request *http.Request){
-    claDemo := &ClassDemo{rp:reponse,rq:request}
-    claDemo.Index();
-}
 
+//基类
 type ClassDemo struct{
     rp http.ResponseWriter
     rq *http.Request    
 }
 
-func (obj *ClassDemo) Index(){
-    fmt.Fprint(obj.rp,"OK")    
+//路由分发
+func Demo(reponse http.ResponseWriter, request *http.Request){
+    claDemo := &ClassDemo{rp:reponse,rq:request}
+    query := request.URL.Query()
+    act := query.Get("f")
+    switch act{
+        case "index":
+            claDemo.index()
+        case "demo":
+            claDemo.demo()
+        default:
+            claDemo.index()    
+    }
+    
+}
+
+func (obj *ClassDemo) index(){
+    fmt.Fprint(obj.rp, "index")
+}
+
+func (obj *ClassDemo) demo() {
+    fmt.Fprint(obj.rp, "demo")
 }
